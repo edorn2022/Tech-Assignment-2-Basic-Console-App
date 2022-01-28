25a658fcc700a971be13c32d9a64c7772e2d819e
edorn <edorn@DESKTOP-CC2QDI4>
2022-01-28 13:57:53 -0500

    Add project files.

---
+++ Tech Assignment 2 Basic Console App/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+/*Eric Dorn
+ * 
+ * 
+ */
+namespace Tech_Assignment_2_Basic_Console_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("There was once a Roman emperor who died because he was struck by lightning breifly after ascending the throne.");
+            Console.WriteLine("Imagine that, you finally become emperor after so long and you get struck by lightning");
+            Console.WriteLine("If you ever feel cheated out of something just remember, at least you are not being fried by 1000's of volts of electricty after achieving your career goal ");
+            Console.WriteLine("Submitted by Eric Dorn");
+        }
+    }
+}
